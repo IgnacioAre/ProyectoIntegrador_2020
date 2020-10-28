7c1b78056567e448b7cc23f2c9852e8a727295b5
Santiago Chúa <66270501+SantiChua@users.noreply.github.com>
2020-10-28 14:55:09 -0300

    probando git

--- ProyectoIntegrador_2020/Notas.vb
+++ ProyectoIntegrador_2020/Notas.vb
@@ -147,4 +147,8 @@ Public Class Notas
             txtNota.ScrollBars = False
         End If
     End Sub
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+
+    End Sub
 End Class
